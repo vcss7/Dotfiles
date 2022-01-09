e5ecf6b5d5d446e2e51f5221b0ddabc295d26cf9
victor-solis <vs.victorsolis@gmail.com>
2022-01-08 23:33:21 -0800

    identify image resolutions

--- local/dev/sort-imgs-by-aspect-ratio.sh
+++ local/dev/sort-imgs-by-aspect-ratio.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # check for dependencies
-if [ ! "$(command -v exiv2)" ]; then
-    echo "Please install exiv2"
+if [ ! "$(command -v magick identify)" ]; then
+    echo "Please install imagemagick"
     exit 1
 fi
 
@@ -11,3 +11,16 @@ DIRECTORY="$HOME/Pictures/wallpapers"
 # check for directory
 [ -d "$DIRECTORY" ] || mkdir -p ~/Pictures/wallpapers
 
+
+# check all for images
+find . -type f | while read -r f
+do
+    # TODO: Fix this atrocity
+    if identify "$f" > /dev/null 2>&1; then
+        RES="$(identify "$f" | awk '{print $3}' | sed 's/x/ /')"
+        WIDTH="$(echo "$RES" | awk '{print $1}')"
+        HEIGHT="$(echo "$RES" | awk '{print $2}')"
+    fi
+done
+
+# 
